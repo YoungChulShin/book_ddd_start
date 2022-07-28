1c9461a00567f134364efcbaa5373764ddb84225
YoungChulShin <go1323@gmail.com>
2022-07-28 23:25:09 +0900

    add orderline initializer

--- 2nd/myshop/src/main/kotlin/study/ddd/myshop/order/command/domain/Order.kt
+++ 2nd/myshop/src/main/kotlin/study/ddd/myshop/order/command/domain/Order.kt
@@ -1,9 +1,33 @@
 package study.ddd.myshop.order.command.domain
 
 class Order(
+    orderLines: List<OrderLine>,
     var state: OrderState,
     var shippingInfo: ShippingInfo
 ) {
+    private lateinit var orderLines: List<OrderLine>
+    private var totalAmounts: Int = 0
+
+    init {
+        setOrderLines(orderLines)
+    }
+
+    private fun setOrderLines(orderLines: List<OrderLine>) {
+        verifyAtLeastOneOrMoreOrderLines(orderLines)
+        this.orderLines = orderLines
+        calculateTotalAmounts()
+    }
+
+    private fun verifyAtLeastOneOrMoreOrderLines(orderLines: List<OrderLine>) {
+        if (orderLines.isEmpty()) {
+            throw IllegalArgumentException("no OrderLine")
+        }
+    }
+
+    private fun calculateTotalAmounts() {
+        this.totalAmounts = orderLines.sumOf { it.amounts }
+    }
+
 
     fun changeShippingInfo(shippingInfo: ShippingInfo) {
         if (isShippingChangeable()) {
